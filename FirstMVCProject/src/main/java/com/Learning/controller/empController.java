085b869aff5575ad0aa052bcb73863d5c0f4123b
Ankit Raj <119001934+Ankitraj5614@users.noreply.github.com>
2026-06-27 23:28:10 +0530

    adding web tool in pom.xml

--- FirstMVCProject/src/main/java/com/Learning/controller/empController.java
+++ FirstMVCProject/src/main/java/com/Learning/controller/empController.java
@@ -1,5 +1,8 @@
 package com.Learning.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,13 +15,24 @@ public class empController {
 	@GetMapping("/")
 	public String homePage(Model model) {
 //		model.addAttribute("msg", "hi i am don ");
-		
-		Employee e = new Employee();
-		e.setEmail("aad@gmail.com");
-		e.setMob("344293");
-		e.setName("ankit");
-		e.setRole("342");
-		model.addAttribute("emp", e);
+//		
+//		Employee e = new Employee();
+//		e.setEmail("raid@gmail.com");
+//		e.setMob("344293");
+//		e.setName("Rohit");
+//		e.setRole("321");
+		List<Employee> employees = Arrays.asList(
+			    new Employee("Ankit Raj", "ankit.raj@gmail.com", "Software Engineer", "9876543210"),
+			    new Employee("Rohit", "priya.sharma@gmail.com", "HR Manager", "9876543211"),
+			    new Employee("Rahul Verma", "rahul.verma@gmail.com", "Backend Developer", "9876543212"),
+			    new Employee("Sneha Gupta", "sneha.gupta@gmail.com", "Frontend Developer", "9876543213"),
+			    new Employee("Amit Kumar", "amit.kumar@gmail.com", "QA Engineer", "9876543214")
+			);
+		model.addAttribute("employees", employees);
 		return "home";
+	}
+	@GetMapping("/register1")
+	public String registerPage() {
+		return "register";
 	}
 }
